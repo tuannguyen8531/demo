141f4e5f653bf4d8387f7e35bce0fd2b3b8e80f6
SeventhS <tuannguyen8531@gmail.com>
2024-07-01 16:44:02 +0700

    fix: fix repository queries

--- server/src/main/java/com/dev/server/repository/TutorialRepository.java
+++ server/src/main/java/com/dev/server/repository/TutorialRepository.java
@@ -9,18 +9,18 @@ import org.springframework.data.jpa.repository.Query;
 import com.dev.server.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
-    @Query("SELECT t FROM Tutorial t WHERE t.deleted_flg = false")
+    @Query("SELECT t FROM Tutorial t WHERE t.deletedFlg = false")
     List<Tutorial> findAllTutorials();
 
-    @Query("SELECT t FROM Tutorial t WHERE t.published = true AND t.deleted_flg = false")
+    @Query("SELECT t FROM Tutorial t WHERE t.published = true AND t.deletedFlg = false")
     List<Tutorial> findTutorialByPublished(boolean published);
 
-    @Query("SELECT t FROM Tutorial t WHERE t.title LIKE %?1% AND t.deleted_flg = false")
+    @Query("SELECT t FROM Tutorial t WHERE t.title LIKE %?1% AND t.deletedFlg = false")
     List<Tutorial> findTutorialByTitleContaining(String title);
 
-    @Query("SELECT t FROM Tutorial t WHERE t.title = ?1 AND t.deleted_flg = false")
+    @Query("SELECT t FROM Tutorial t WHERE t.title = ?1 AND t.deletedFlg = false")
     Optional<Tutorial> findTutorialByTitle(String title);
 
-    @Query("SELECT t FROM Tutorial t WHERE t.id = ?1 AND t.deleted_flg = false")
+    @Query("SELECT t FROM Tutorial t WHERE t.id = ?1 AND t.deletedFlg = false")
     Optional<Tutorial> findTutorialById(Long id);
 }
