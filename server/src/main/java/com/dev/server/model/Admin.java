800737cc8e644181f24c76f9a88fa61563787f98
SeventhS <tuannguyen8531@gmail.com>
2024-07-08 16:43:52 +0700

    feat: authorization

--- server/src/main/java/com/dev/server/model/Admin.java
+++ server/src/main/java/com/dev/server/model/Admin.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.*;
@@ -67,7 +68,7 @@ public class Admin implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     @Override public String getUsername() { return username; }
